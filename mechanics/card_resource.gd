ad8a7b1dc5fb29fa8b422d7d986008f590bf1d14
Mode:Rogue <moderoguegames@gmail.com>
2025-03-18 22:30:50 -0400

    Updated Card Resource
    Added a card weight stat to the cards, added an update in the resource file showing each current weight in one location

--- mechanics/card_resource.gd
+++ mechanics/card_resource.gd
@@ -1,5 +1,17 @@
 class_name CardResource extends Resource
 
+## List of CARD WEIGHTS from 3/18 - Jintachi
+#	1.0		1,2,3,4,5,6
+#	0.9		8, 9
+#	0.8		
+#	0.7		
+#	0.6		7
+#	0.5		
+#	0.4		BAR
+#   0.3		TREASURE
+#   0.2		BOMB
+#   0.1		
+
 enum CardType {
 	BASE,
 	TREASURE,
@@ -12,3 +24,4 @@ enum CardType {
 @export var card_texture_position : int = 0 ## The texture of the card.
 ## What type of card it is
 @export var card_type : CardType
+@export var card_weight : float = 1.0 ## Default card weight for mapping.
