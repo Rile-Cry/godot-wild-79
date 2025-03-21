25f7429689735b64858a9c3c2b4c4963425874e5
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-03-20 17:04:39 -0700

    ui

--- scenes/mockup.gd
+++ scenes/mockup.gd
@@ -8,17 +8,17 @@ func _on_button_pressed() -> void:
 	lever.play("default")
 	
 
-
+# TODO make it so you can't press the info button while the side panel is moving
 func _on_info_button_pressed() -> void:
-	var tween = get_tree().create_tween()
-	if sidePanel.position.x > 0 and !tween.is_running():
-		tween.tween_property(sidePanel, "position:x", 0, .5)
-		tween.play()
-		tween.stop()
-	elif sidePanel.position.x == 0 and !tween.is_running():
-		tween.tween_property(sidePanel, "position:x", 70, .5)
-		tween.play()
-		tween.stop()
+	var tween_right = get_tree().create_tween()
+	var tween_left = get_tree().create_tween()
+	if sidePanel.position.x > 0:
+		tween_left.tween_property(sidePanel, "position:x", 0, .35)
+		tween_left.play()
+		
+	elif sidePanel.position.x == 0:
+		tween_right.tween_property(sidePanel, "position:x", 70, .35)
+		tween_right.play()
 			
 			
 			
