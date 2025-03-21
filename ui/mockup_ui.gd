537e20715d507e1f0dba2a58d70a1cff7c0c55de
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-03-20 21:41:54 -0700

    side panel movement and visibility functioning
    At this point, the side panel is visible, and it moves, however you are able to click the info button multiple times while the side panel is travelling which causes a bug

--- ui/mockup_ui.gd
+++ ui/mockup_ui.gd
@@ -1,5 +1,18 @@
 extends CanvasLayer
 @onready var lever = $Lever
+@onready var leverPull = $LeverPull
+@onready var sidePanel = $SidePanel
 
 func _on_lever_button_pressed() -> void:
 	lever.play("default")
+	leverPull.play()
+
+
+func _on_info_button_pressed() -> void:
+	var tween = get_tree().create_tween()
+	if sidePanel.offset.x > 0:
+		tween.tween_property(sidePanel, "offset:x", 0, .5)
+		tween.play()
+	elif sidePanel.offset.x == 0:
+		tween.tween_property(sidePanel, "offset:x", 70, .5)
+		tween.play()
