ac0b39e2ec1eaf0713530d00fcb1e3c12529d683
Mode:Rogue <moderoguegames@gmail.com>
2025-03-23 00:39:29 -0400

    signals for bonus and lvlups

--- ui/power_ups.gd
+++ ui/power_ups.gd
@@ -40,8 +40,7 @@ func _ready() -> void:
 func update_bonus(new_bonus: Genum.BonusType, new_level: Genum.BonusLevel) -> void:
 	bonus = new_bonus
 	level = new_level
-	GameGlobalEvents.bonus_level_sound.emit(level)
-	
+	GameGlobalEvents.bonus_level_sound.emit(level)	
 	
 	var bonus_node : TextureRect = $VBoxContainer/HBoxContainer/TextureRect.duplicate()
 	#bonus_node.position.y += v_offset
