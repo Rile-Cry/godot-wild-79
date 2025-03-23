373c83a1bcb0cea365e68d6b4bd3f0bc5e4ee0ae
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-03-22 23:42:36 -0700

    Update main_menu.gd

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -3,7 +3,11 @@ extends NinePatchRect
 
 func _on_button_pressed() -> void:
 	var tween = get_tree().create_tween()
-	tween.tween_property(self, "scale", Vector2.ZERO, .5)
+	tween.tween_property(self, "position:y", -50, .2)
 	tween.play()
 	await tween.finished
+	var tween1 = get_tree().create_tween()
+	tween1.tween_property(self, "position:y", 200, .75)
+	tween1.play()
+	await tween1.finished
 	GameGlobalEvents.game_start.emit()
