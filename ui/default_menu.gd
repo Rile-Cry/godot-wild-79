538b438958f9558a1fb02de2dcfa56e6f327e3ac
Zach Carter <69975691+zcarter95@users.noreply.github.com>
2025-03-29 12:53:42 -0700

    Options menu work (volume)

--- ui/default_menu.gd
+++ ui/default_menu.gd
@@ -5,6 +5,10 @@ extends NinePatchRect
 @export var sfx_volume : VSlider
 @export var new_run_button : TextureButton
 @export var res_button : TextureButton
+@onready var _master_bus := AudioServer.get_bus_index("Master")
+@onready var _sfx_bus := AudioServer.get_bus_index("SFX")
+@onready var _music_bus := AudioServer.get_bus_index("Music")
+
 
 func _ready() -> void:
 	#Wwise.set_rtpc_value_id(AK.GAME_PARAMETERS.MASTERVOL, master_volume.value, null)
@@ -12,18 +16,18 @@ func _ready() -> void:
 	#Wwise.set_rtpc_value_id(AK.GAME_PARAMETERS.SFXVOL, sfx_volume.value, null)
 	
 	res_button.pressed.connect(ScreenManager.change_size)
+	master_volume.value = db_to_linear(AudioServer.get_bus_volume_db(_master_bus))
+	sfx_volume.value = db_to_linear(AudioServer.get_bus_volume_db(_sfx_bus))
+	music_volume.value = db_to_linear(AudioServer.get_bus_volume_db(_music_bus))
 
 func _on_master_changed(value: float) -> void:
-	pass
-	#Wwise.set_rtpc_value_id(AK.GAME_PARAMETERS.MASTERVOL, value, null)
+	AudioServer.set_bus_volume_db(_master_bus, linear_to_db(value))
 
 func _on_music_changed(value: float) -> void:
-	#Wwise.set_rtpc_value_id(AK.GAME_PARAMETERS.OSTVOL, value, null)
-	pass
+	AudioServer.set_bus_volume_db(_music_bus, linear_to_db(value))
 
 func _on_sfx_changed(value: float) -> void:
-	#Wwise.set_rtpc_value_id(AK.GAME_PARAMETERS.SFXVOL, value, null)
-	pass
+	AudioServer.set_bus_volume_db(_sfx_bus, linear_to_db(value))
 
 func _on_new_start_pressed() -> void:
 	get_tree().paused = false
@@ -32,3 +36,7 @@ func _on_new_start_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	GameGlobalEvents.exit_game.emit()
+
+
+func _on_resolution_button_pressed() -> void:
+	ScreenManager.change_size()
